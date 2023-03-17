3f2d77e11290dd498ecdbd09e154ac3a09dc5091
RamaRamdani03 <ramaramdani449@gamil.com>
2023-03-17 16:02:03 +0700

    menambah variabel ke 1

--- bublesort/bublesort.cpp
+++ bublesort/bublesort.cpp
@@ -2,6 +2,26 @@
 #include <iostream>
 using namespace std;
 
-int a[20];				//Deklarasi array a dengan ukuran 20
-int n;					//Deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
+int a[20];				// Deklarasi array a dengan ukuran 20
+int n;					// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
 
+void input() {			// Prosedur untuk input
+	while (true) {		// Looping
+		cout << "Masukan banyaknya elemen pada array : "; //output ke layar
+		cin >> n;		// Input dari pengguna 
+		if (n <= 20)	// Jika n kurang dari atau sama dengan 20
+			break;		// Keluar dari Loop
+		else {			// Jikan n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Output 
+		}
+	}
+	cout << endl;							// Output baris kosong 
+	cout << "==========" << endl;			// Output ke layar
+	cout << "Masukan Elemen Array" << endl;	// Output ke layar
+	cout << "==========" << endl;			// Output ke layar
+
+	for (int i = 0; i < n; i++) {	// Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";	// Output ke layar
+		cin >> a[i];
+	}
+}
